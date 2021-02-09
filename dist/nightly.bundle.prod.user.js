1a5b340574834c4962153e08612382c60ab6a2e0
Andy-K-Sparklight <Twilight_Princess_Sparkle@outlook.com>
2021-02-09 18:14:13 +0800

    build(loader): update version to 1.1.0

--- dist/nightly.bundle.prod.user.js
+++ dist/nightly.bundle.prod.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         MCBBS Loader
 // @namespace    https://mcbbs-loader.xuogroup.top
-// @version      1.0.1
+// @version      1.1.0
 // @description  Load custom modules into MCBBS!
 // @author       mcbbs-loader
 // @match        http*://www.mcbbs.net/*
